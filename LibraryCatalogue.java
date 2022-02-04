1499e4662657e07007c0d3d6fa6de2cfb602f984
Madhav-codr <74365797+Madhav-codr@users.noreply.github.com>
2022-02-04 18:18:31 +0530

    Update LibraryCatalogue.java

--- LibraryCatalogue.java
+++ LibraryCatalogue.java
@@ -80,18 +80,4 @@ public class LibraryCatalogue {
     public void sorryBookAlreadyCheckedOut(Book book) {
         System.out.println("Sorry, " + book.getTitle() + " is already out. It would be back on " + (getLengthOfCheckOutPeriod() + book.getDayCheckedOut()) + " .");
     }
-
-    public static void main(String[] args) {
-        Map<String, Book> bookCollection = new HashMap<String, Book>();
-        Book harry = new Book("Harry Porter" , 782322 , 23232334);
-        bookCollection.put("Harry Porter", harry);
-        LibraryCatalogue lib = new LibraryCatalogue(bookCollection);
-        lib.checkOutBook("Harry Porter");
-        lib.nextDay();
-        lib.nextDay();
-        lib.checkOutBook("Harry Porter");
-        lib.setDay(17);
-        lib.returnBook("Harry Porter");
-        lib.checkOutBook("Harry Porter");
-    }
 }

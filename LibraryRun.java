42c47bb523a4e09028086716589c56c788a9a41e
Madhav-codr <74365797+Madhav-codr@users.noreply.github.com>
2022-02-04 18:39:32 +0530

    Update LibraryRun.java

--- LibraryRun.java
+++ LibraryRun.java
@@ -1,5 +1,5 @@
-import java..LiraryCatalogue;
-public class TryRun {
+import java.Library.LiraryCatalogue;
+public class LibraryRun {
 	public static void main(String[] args) {
         Map<String, Book> bookCollection = new HashMap<String, Book>();
         Book harry = new Book("Harry Porter" , 782322 , 23232334);

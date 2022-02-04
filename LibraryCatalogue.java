0fb3451f250043124526adaa33843dd56f77c82a
Madhav-codr <74365797+Madhav-codr@users.noreply.github.com>
2022-02-04 18:14:56 +0530

    Add files via upload

---
+++ LibraryCatalogue.java
@@ -0,0 +1,97 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LibraryCatalogue {
+
+    // PROPERTIES, FIELDS, GLOBAL VARIABLES
+    Map<String,Book> bookCollection = new HashMap<String, Book>();
+    int currentDay = 0;
+    int lengthOfCheckOutPeriod = 7;
+    double initialLateFee = 10;
+    double feePerLateDay = 5;
+
+    // CONSTRUCTORS
+    public LibraryCatalogue(Map<String,Book> collection ) {
+        this.bookCollection = collection;
+    }
+    public LibraryCatalogue(Map<String,Book> collection, int lengthOfCheckOutPeriod, double initialLateFee, int feePerLateDay ) {
+        this.bookCollection =collection;
+        this.lengthOfCheckOutPeriod = lengthOfCheckOutPeriod;
+        this.initialLateFee = initialLateFee;
+        this.feePerLateDay = feePerLateDay;
+    }
+
+    // GETTERS
+    public int getCurrentDay() {
+        return this.currentDay;
+    }
+
+    public Map<String, Book> getBookCollection() {
+        return this.bookCollection;
+    }
+
+    public Book getBook(String bookTitle) {
+        return getBookCollection().get(bookTitle);
+    }
+
+    public int getLengthOfCheckOutPeriod() {
+        return this.lengthOfCheckOutPeriod;
+    }
+
+    public double getInitialLateFee() {
+        return this.initialLateFee;
+    }
+
+    public double getFeePerLateDay() {
+        return this.feePerLateDay;
+    }
+
+    // SETTERS
+    public void nextDay() {
+        currentDay++;
+    }
+
+    public void setDay(int day) {
+        currentDay = day;
+    }
+
+    // INSTANCE METHODS
+    public void checkOutBook(String title) {
+        Book book = getBook(title);
+        if (book.getIsCheckedOut()) {
+            sorryBookAlreadyCheckedOut(book);
+        }else {
+            book.setIsCheckedOut(true, currentDay);
+            System.out.println("You got checked out " + title + ". It is due on day " + (getCurrentDay() + getLengthOfCheckOutPeriod()) + ".");
+        }
+    }
+
+    public void returnBook(String title) {
+        Book book = getBook(title);
+        int daysLate = currentDay - (book.getDayCheckedOut() + getLengthOfCheckOutPeriod());
+        if (daysLate > 0) {
+            System.out.println("You owe to the library ₹" + (getInitialLateFee()+ daysLate * getFeePerLateDay()) + " because your book is " + daysLate + " days overdue.");
+        }else {
+            System.out.println("Book returned. Thank you.");
+        }
+        book.setIsCheckedOut(false, -1);
+    }
+
+    public void sorryBookAlreadyCheckedOut(Book book) {
+        System.out.println("Sorry, " + book.getTitle() + " is already out. It would be back on " + (getLengthOfCheckOutPeriod() + book.getDayCheckedOut()) + " .");
+    }
+
+    public static void main(String[] args) {
+        Map<String, Book> bookCollection = new HashMap<String, Book>();
+        Book harry = new Book("Harry Porter" , 782322 , 23232334);
+        bookCollection.put("Harry Porter", harry);
+        LibraryCatalogue lib = new LibraryCatalogue(bookCollection);
+        lib.checkOutBook("Harry Porter");
+        lib.nextDay();
+        lib.nextDay();
+        lib.checkOutBook("Harry Porter");
+        lib.setDay(17);
+        lib.returnBook("Harry Porter");
+        lib.checkOutBook("Harry Porter");
+    }
+}
